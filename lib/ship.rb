a0c0d747dcaeafb7e0491fb34e9f5fc24c85f41d
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-03-30 12:51:59 -0700

    Create sunk method

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,9 +1,17 @@
 class Ship
   attr_reader :name,
                    :health
-                   
+
   def initialize(name, health)
     @name = name
     @health = health
+  end
+
+  def sunk?
+    if @health == 0
+      true
+    else
+      false
+    end
   end
 end
