0cb91a9ac5c5427e7f884f779fc84168c9ec1341
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-03-30 15:38:21 -0700

    Create length instance variable

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,10 +1,12 @@
 class Ship
   attr_reader :name,
-                   :health
+                   :health,
+                   :length
 
   def initialize(name, health)
     @name = name
     @health = health
+    @length = health
   end
 
   def sunk?
