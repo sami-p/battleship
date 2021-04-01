3556005544a49011cf62e32507f5d79c6ba7616f
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-03-31 22:02:11 -0700

    Create valid placement specs

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -26,4 +26,32 @@ describe Board do
       expect(board.valid_coordinate?("A22")).to eq(false)
     end
   end
+
+  describe 'valid placement' do
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+
+    it 'can validate coordinates to false' do
+      expect(board.valid_placement?(cruiser, ["A1", "A2"])).to eq(false)
+      expect(board.valid_placement?(submarine, ["A2", "A3", "A4"])).to eq(false)
+    end
+
+    it 'coordinates must be consecutive' do
+      expect(board.valid_placement?(cruiser, ["A1", "A2", "A4"])).to eq(false)
+      expect(board.valid_placement?(submarine, ["A1", "C1"])).to eq(false)
+      expect(board.valid_placement?(cruiser, ["A3", "A2", "A1"])).to eq(false)
+      expect(board.valid_placement?(submarine, ["C1", "B1"])).to eq(false)
+    end
+
+    xit 'coordinates can not be diagonal' do
+      expect(board.valid_placement?(cruiser, ["A1", "B2", "C3"])).to eq(false)
+      expect(board.valid_placement?(submarine, ["C2", "D3"])).to eq(false)
+    end
+
+    xit 'coordinates to true' do
+      expect(board.valid_placement?(submarine, ["A1", "A2"])).to eq(true)
+      expect(board.valid_placement?(cruiser, ["B1", "C1", "D1"])).to eq(true)
+    end
+  end
 end
