a2c617bdb28cc15f4db60e9ee8864b03f9c058d0
sami-p <sami.peterson14@gmail.com>
2021-03-31 20:25:06 -0500

    Add board spec

---
+++ spec/board_spec.rb
@@ -0,0 +1,15 @@
+require 'rspec'
+require './lib/ship'
+require './lib/board'
+require './lib/cell'
+
+describe Board do
+  describe 'initialize' do
+    board = Board.new
+    it 'exists' do
+      expect(board).to be_a Board
+    end
+
+    
+  end
+end
