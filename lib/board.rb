fd927abc4c8958e20cb92646fcbf9803d434a41c
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-04-02 20:42:51 -0700

    Add nil? to valid placement method

--- lib/board.rb
+++ lib/board.rb
@@ -37,7 +37,7 @@ class Board
   end
 
   def letter_cons
-    @letter_array.each_cons(2).all? { |x, y| x == y } || @letter_array.each_cons(2).all? { |x, y| x == y - 1 }
+    @letter_array.each_cons(2).all? { |x, y| x == y- 1 } || @letter_array.each_cons(2).all? { |x, y| x == y  }
   end
 
   def num_cons
@@ -49,7 +49,7 @@ class Board
   end
 
   def valid_placement?(ship, coordinates)
-    if ship.length == coordinates.length
+    if ship.length == coordinates.length && @cells[coordinates].nil?
       ord_arrays(coordinates)
         return true if (letter_cons && num_cons == true) && (diagonal_cons == false)
         else
@@ -59,26 +59,3 @@ class Board
       false
   end
 end
-
-# Split into two methods
-# use each_con to iterate through numbers or letter
-# at each index it needs to be <<ed into an array
-# use documention around arthimatic - this can help with the sussing out of the logic
-# Ensure the ships don't overlap - empty?
-# We need to ensure that everything is -1 from each other
-# Use each_con(2) to check if the horizontal letters and numbers were true.
-# Need to check if the string "letter" is in order or the same.
-# Need to check is the string "integer" consecutive
-
-# Method checks whether player input valid coordinates, and the ship's placement was either horizontal or vertical
-# def valid_placement?(ship, coordinates)
-#   player_input_valid?(ship, coordinates) &&
-#   (is_horizontal?(ship, coordinates) ||
-#   is_vertical?(ship, coordinates))
-# end
-
-  # def consecutive(ship, coordinates)
-  # end
-
-  # def not_diagonal(ship, coordinates)
-  # end
