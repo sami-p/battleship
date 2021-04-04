e8f316d06823b9d73c5bf4fda1d870f007041910
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-04-04 00:04:22 -0700

    Create specs to build Game class off of

---
+++ spec/game_spec.rb
@@ -0,0 +1,30 @@
+require 'rspec'
+require './lib/ship'
+require './lib/cell'
+require './lib/board'
+require './lib/game'
+
+describe Game do
+  describe 'initialize' do
+    board = Board.new
+    game = Game.new(board)
+
+    it 'exists' do
+      expect(game).to be_an_instance_of(Game)
+    end
+
+    it 'has a board' do
+      expect(game.board.cells.count).to eq(16)
+    end
+  end
+
+  describe 'start' do
+    board = Board.new
+    game = Game.new(board)
+
+    it 'can render a board' do
+      expected = " 1 2 3 4 \nA . . . .\nB . . . .\nC . . . .\nD . . . .\n"
+      expect(game.start).to eq(expected)
+    end
+  end
+end
