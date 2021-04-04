89374064927967480b3f1010a3b139b0388afb00
sami-p <sami.peterson14@gmail.com>
2021-04-04 18:28:41 -0500

    Initialize turn spec

---
+++ spec/turn_spec.rb
@@ -0,0 +1,17 @@
+require 'rspec'
+require './lib/ship'
+require './lib/cell'
+require './lib/board'
+require './lib/game'
+require './lib/turn'
+
+describe Turn do
+  describe "initialize" do
+    board = Board.new
+    game = Game.new(board)
+
+    it "exists" do
+      expect(turn).to be an_instance_of(Turn)
+    end
+  end
+end
