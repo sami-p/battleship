da0325544aefe8b92af75646327addd05c146be8
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-04-05 20:16:22 -0700

    Create place ship method

--- lib/carl_computer.rb
+++ lib/carl_computer.rb
@@ -1,23 +1,36 @@
-require './lib/ship'
-require './lib/cell'
-require './lib/board'
-require 'pry'
-
 class CarlComputer
-  attr_accessor :ship_2,
-                   :ship_3,
-                   :carl_board,
+  attr_accessor :carl_board,
                    :carl_shots,
-                   :cruiser_coordinates,
-                   :sub_coordinates
+                   :ship_2,
+                   :ship_3,
+                   :ships
 
-  def initialize
+  def initialize(carl_board)
+    @carl_board = carl_board
+    @carl_shots = @carl_board.cells.keys
     @ship_2 = Ship.new('Submarine', 2)
     @ship_3 = Ship.new('Cruiser', 3)
-    @carl_board = Board.new
-    @carl_shots = @carl_board.cells.keys
-    @cruiser_coordinates = []
-    @sub_coordinates = []
+    @ships = []
+  end
+
+  def add_ships
+    @ships << @ship_2
+    @ships << @ship_3
+    @ships
+  end
+
+  def carl_ship_coordinates(ship, length)
+    coordinates = []
+    until @carl_board.valid_placement?(ship, coordinates)
+      comp_coordinates = @carl_board.cells.keys.sample(length)
+      coordinates = comp_coordinates
+    end
+    coordinates
+  end
+
+  def computer_place_ship(ship)
+    coordinates = carl_ship_coordinates(ship, ship.length)
+    @carl_board.place(ship, coordinates)
   end
 
   def carl_fires
@@ -26,40 +39,39 @@ class CarlComputer
     @carl_board.cells[guess].fire_upon
     @carl_shots.delete(guess)
   end
-
-  def carl_horizontal_cruiser
-    @carl_shots.each_cons(3) { |coords| @cruiser_coordinates << coords }
-  end
-
-  def carl_vert_cruiser
-    vertical = @carl_shots.sort_by do |vertic|
-      vertic[1]
-    end
-    vertical.each_cons(2) { |vert| @cruiser_coordinates << vert }
-  end
-
-  def carl_horizontal_sub
-    @carl_shots.each_cons(3) { |coords| @sub_coordinates << coords }
-  end
-
-  def carl_vert_sub
-    vertical = @carl_shots.sort_by do |vertic|
-      vertic[1]
-    end
-    vertical.each_cons(2) { |vert| @sub_coordinates << vert }
-  end
-
-  def cruiser_place
-    until @carl_board.valid_placement?(@ship_3, coordinates = @cruiser_coordinates.sample)
-      loop
-    end
-    @carl_board.place(@ship_3, coordinates)
-  end
-
-  def sub_place
-    until @carl_board.valid_placement?(@ship_2, coordinates = @sub_coordinates.sample)
-      loop
-    end
-    @carl_board.place(@ship_2, coordinates)
-  end
 end
+  # def carl_horizontal_cruiser
+  #   @carl_shots.each_cons(3) { |coords| @cruiser_coordinates << coords }
+  # end
+  #
+  # def carl_vert_cruiser
+  #   vertical = @carl_shots.sort_by do |vertic|
+  #     vertic[1]
+  #   end
+  #   vertical.each_cons(2) { |vert| @cruiser_coordinates << vert }
+  # end
+  #
+  # def carl_horizontal_sub
+  #   @carl_shots.each_cons(3) { |coords| @sub_coordinates << coords }
+  # end
+  #
+  # def carl_vert_sub
+  #   vertical = @carl_shots.sort_by do |vertic|
+  #     vertic[1]
+  #   end
+  #   vertical.each_cons(2) { |vert| @sub_coordinates << vert }
+  # end
+  #
+  # def cruiser_place
+  #   until @carl_board.valid_placement?(@ship_3, coordinates = @cruiser_coordinates.sample)
+  #     loop
+  #   end
+  #   @carl_board.place(@ship_3, coordinates)
+  # end
+  #
+  # def sub_place
+  #   until @carl_board.valid_placement?(@ship_2, coordinates = @sub_coordinates.sample)
+  #     loop
+  #   end
+  #   @carl_board.place(@ship_2, coordinates)
+  # end
