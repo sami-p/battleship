47be4a9fca7d5f71a25699249ac61127b974c88b
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-04-05 22:09:15 -0700

    Update initialize

--- lib/carl_computer.rb
+++ lib/carl_computer.rb
@@ -40,38 +40,4 @@ class CarlComputer
     @carl_shots.delete(guess)
   end
 end
-  # def carl_horizontal_cruiser
-  #   @carl_shots.each_cons(3) { |coords| @cruiser_coordinates << coords }
-  # end
-  #
-  # def carl_vert_cruiser
-  #   vertical = @carl_shots.sort_by do |vertic|
-  #     vertic[1]
-  #   end
-  #   vertical.each_cons(2) { |vert| @cruiser_coordinates << vert }
-  # end
-  #
-  # def carl_horizontal_sub
-  #   @carl_shots.each_cons(3) { |coords| @sub_coordinates << coords }
-  # end
-  #
-  # def carl_vert_sub
-  #   vertical = @carl_shots.sort_by do |vertic|
-  #     vertic[1]
-  #   end
-  #   vertical.each_cons(2) { |vert| @sub_coordinates << vert }
-  # end
-  #
-  # def cruiser_place
-  #   until @carl_board.valid_placement?(@ship_3, coordinates = @cruiser_coordinates.sample)
-  #     loop
-  #   end
-  #   @carl_board.place(@ship_3, coordinates)
-  # end
-  #
-  # def sub_place
-  #   until @carl_board.valid_placement?(@ship_2, coordinates = @sub_coordinates.sample)
-  #     loop
-  #   end
-  #   @carl_board.place(@ship_2, coordinates)
-  # end
+  
