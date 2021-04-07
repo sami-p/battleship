f53e10e09df447df6f308a6166d9954eb0584d2a
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-04-07 00:03:02 -0700

    Comment out tests until use

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -1,24 +1,24 @@
-require 'rspec'
-require './lib/ship'
-require './lib/cell'
-require './lib/board'
-require './lib/game'
-require './lib/turn'
-
-describe Turn do
-  describe "initialize" do
-    board = Board.new
-    game = Game.new(board)
-    turn = Turn.new
-
-    it "exists" do
-      expect(turn).to be_an_instance_of(Turn)
-    end
-  end
-
-  describe "turns" do
-    it "takes user's turns" do
-        turn = Turn.new
-    end
-  end
-end
+# require 'rspec'
+# require './lib/ship'
+# require './lib/cell'
+# require './lib/board'
+# require './lib/game'
+# require './lib/turn'
+#
+# describe Turn do
+#   describe "initialize" do
+#     board = Board.new
+#     game = Game.new(board)
+#     turn = Turn.new
+#
+#     it "exists" do
+#       expect(turn).to be_an_instance_of(Turn)
+#     end
+#   end
+#
+#   describe "turns" do
+#     it "takes user's turns" do
+#         turn = Turn.new
+#     end
+#   end
+# end
