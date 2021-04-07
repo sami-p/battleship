19b829a1f8296e7e99adf863e537ea71670f788e
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-04-07 11:52:32 -0700

    Add spec for player shots

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -25,6 +25,11 @@ describe Game do
       expect(carl_computer.carl_board.cells.count).to eq(16)
     end
 
+    it 'can have player shots' do
+      expected = ["A1", "A2", "A3", "A4", "B1", "B2", "B3", "B4", "C1", "C2", "C3", "C4", "D1", "D2", "D3", "D4"]
+      expect(game.player_shots).to eq(expected)
+    end
+
     it 'can not cheat by defalut' do
       expect(game.cheat).to eq(false)
     end
